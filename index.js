eabd6d38681baacfe8b2ba90888dc2fb3fd87bab
Shuai Yu <claudeyu.89@gmail.com>
2017-08-18 13:52:41 +1000

    delete build setting

--- index.js
+++ index.js
@@ -3,10 +3,4 @@ const app = express();
 const path = require('path');
 require('./server/server');
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + './client/build/index.html'));
-});
-
 module.exports = app;
