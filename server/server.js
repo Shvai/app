70a7a56d074dddf19ea70944dea35361af3990e8
Shuai Yu <claudeyu.89@gmail.com>
2017-08-22 09:58:37 +1000

    server

--- server/server.js
+++ server/server.js
@@ -11,20 +11,24 @@ mongoose.connect(
   'ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
 );
 
+// Serve static files from the React app
 app.use(express.static(path.join('../client/build')));
+
 app.use('/users', UserController);
 
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join('../client/build/index.html'));
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
+/*
 app.use(function (req, res) {
   res.status(404).send({url: req.originalUrl + ' not found'})
 });
+*/
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, function () {
   console.log('Express server listening on port ' + port);
 });
-
-module.exports = app;
