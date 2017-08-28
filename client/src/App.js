48be51bd5624fa3543d51df72362d271809581fa
Shuai Yu <claudeyu.89@gmail.com>
2017-08-29 08:47:53 +1000

    App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,20 +13,24 @@ class App extends Component {
 
   render() {
     return (
-
       <div className="App">
         <h1>Users</h1>
         <table className="myTable">
           <tr className="header">
-            <th style={{ width: '33.333333%' }}>Firstname</th>
-            <th style={{ width: '33.333333%' }}>Lastname</th>
-            <th style={{ width: '33.333333%' }}>Email</th>
+            <th style={{ width: '20%' }}>Firstname</th>
+            <th style={{ width: '20%' }}>Lastname</th>
+            <th style={{ width: '25%' }}>Email</th>
+            <th style={{ width: '35%' }}></th>
           </tr>
           {this.state.users.map(User =>
             <tr key={User.id}>
               <td>{User.firstname}</td>
               <td>{User.lastname}</td>
               <td>{User.email}</td>
+              <td>
+                <button>Edit</button>
+                <button onClick={}>Delete</button>
+              </td>
             </tr>)}
         </table>
       </div>
