669f0183c92fdbd33b03171064fb30608885818a
Shuai Yu <claudeyu.89@gmail.com>
2017-09-22 13:56:01 +1000

    #

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -7,10 +7,12 @@ module.exports = merge(common, {
   devtool: 'source-map',
   plugins: [
     new UglifyJSPlugin(),
+    /*
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
       }
     })
+    */
   ]
 });
