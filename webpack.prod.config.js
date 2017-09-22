f10fd24315c97d71c17c2cd7aea535c753abea27
Shuai Yu <claudeyu.89@gmail.com>
2017-09-22 14:10:46 +1000

    #

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.config.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   devtool: 'source-map',
