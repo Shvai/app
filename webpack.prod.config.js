843b156db0bbf9cca33a16ebb6a85356c6929c09
Shuai Yu <claudeyu.89@gmail.com>
2017-09-26 12:59:43 +1000

    Add source-map for UglifyJS

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,17 +1,14 @@
-const merge = require('webpack-merge');
 const webpack = require('webpack');
-//const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const merge = require('webpack-merge');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.config.js');
 
 module.exports = merge(common, {
   devtool: 'source-map',
   plugins: [
-    //new webpack.optimize.UglifyJsPlugin({
-      //minimize: true,
-      //compress: {
-       // warnings: false
-      //}
-    //}),
+    new UglifyJSPlugin({
+      sourceMap: true
+    }),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
