fd1cc90a0123db60b64b4658b1a88a3970bfdb84
Shuai Yu <claudeyu.89@gmail.com>
2017-09-26 12:59:20 +1000

    remove publicPath

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -8,8 +7,7 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.js',
-    path: __dirname + '/dist',
-    publicPath: '/dist/'
+    path: path.resolve(__dirname, 'dist')
   },
   devServer: {
     contentBase: './dist',
