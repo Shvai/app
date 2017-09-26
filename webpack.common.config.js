23dd1b35daab8f403f60928ab916fad96b5583c3
Shuai Yu <claudeyu.89@gmail.com>
2017-09-26 12:15:04 +1000

    prod config

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: __dirname + '/dist',
-    publicPath: '/'
+    publicPath: '/dist/'
   },
   devServer: {
     contentBase: './dist',
@@ -23,10 +23,15 @@ module.exports = {
       filename: 'index.html',
       inject: 'body'
     }),
-    //new CleanWebpackPlugin(['dist'])
+    new CleanWebpackPlugin(['dist'])
   ],
   module: {
     loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
@@ -34,17 +39,16 @@ module.exports = {
         ]
       },
       {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
         test: /\.css$/,
         use: [
           'style-loader',
           'css-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
         ]
       }
     ]
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,13 +1,17 @@
 const merge = require('webpack-merge');
 const webpack = require('webpack');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+//const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.config.js');
 
 module.exports = merge(common, {
   devtool: 'source-map',
   plugins: [
-    new UglifyJSPlugin(),
-
+    //new webpack.optimize.UglifyJsPlugin({
+      //minimize: true,
+      //compress: {
+       // warnings: false
+      //}
+    //}),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
